0803d5d02b876f3387accc717cda332e3f6248cc
Gabriel Pailamilla <gabrielpailamilla13724@gmail.com>
2024-10-29 17:44:24 -0300

    act
    act v1

--- hotel-boutique-reservation/src/app/catalogo-habitaciones/catalogo-habitaciones.component.ts
+++ hotel-boutique-reservation/src/app/catalogo-habitaciones/catalogo-habitaciones.component.ts
@@ -14,7 +14,7 @@ export class CatalogoHabitacionesComponent {
     id: number;
     nombre: string;
     precio: number;
-    Disponibilidad: number;
+    disponibilidad: number;
     imagenUrl: string;
   }[];
 
@@ -28,45 +28,81 @@ export class CatalogoHabitacionesComponent {
         id: 1,
         nombre: 'Habitación Estandar',
         precio: 89990,
-        Disponibilidad: 5,
+        disponibilidad: 5,
         imagenUrl: 'https://www.ahstatic.com/photos/b5w1_rodbcovz_00_p_1024x768.jpg'
       },
       {
         id: 2,
         nombre: 'Habitación Twin',
         precio: 119990,
-        Disponibilidad: 5,
+        disponibilidad: 5,
         imagenUrl: 'https://www.ahstatic.com/photos/b5w1_rod3cov_00_p_1024x768.jpg'
       },
       {
         id: 3,
         nombre: 'Habitación Superior',
         precio: 159990,
-        Disponibilidad: 5,
+        disponibilidad: 5,
         imagenUrl: 'https://www.ahstatic.com/photos/b5w1_rodbbofz_00_p_1024x768.jpg'
       },
       {
         id: 4,
         nombre: 'Habitación Ejecutiva',
         precio: 129990,
-        Disponibilidad: 5,
+        disponibilidad: 5,
         imagenUrl: 'https://www.ahstatic.com/photos/b5w1_rodbaofz_00_p_1024x768.jpg'
       },
       {
         id: 5,
         nombre: 'Habitación Ejecutiva',
         precio: 129990,
-        Disponibilidad: 5,
+        disponibilidad: 5,
         imagenUrl: 'https://www.ahstatic.com/photos/b5w1_rodbaofz_00_p_1024x768.jpg'
       },
+      {
+        id: 5,
+        nombre: 'Habitación Ejecutiva',
+        precio: 129990,
+        disponibilidad: 5,
+        imagenUrl: 'https://www.ahstatic.com/photos/b5w1_rodbaofz_00_p_1024x768.jpg'
+      },
+      {
+        id: 5,
+        nombre: 'Habitación Ejecutiva',
+        precio: 129990,
+        disponibilidad: 5,
+        imagenUrl: 'https://www.ahstatic.com/photos/b5w1_rodbaofz_00_p_1024x768.jpg'
+      },
+      {
+        id: 5,
+        nombre: 'Habitación Ejecutiva',
+        precio: 129990,
+        disponibilidad: 5,
+        imagenUrl: 'https://www.ahstatic.com/photos/b5w1_rodbaofz_00_p_1024x768.jpg'
+      },
+      {
+        id: 5,
+        nombre: 'Habitación Ejecutiva',
+        precio: 129990,
+        disponibilidad: 5,
+        imagenUrl: 'https://www.ahstatic.com/photos/b5w1_rodbaofz_00_p_1024x768.jpg'
+      },
+      {
+        id: 5,
+        nombre: 'Habitación Ejecutiva',
+        precio: 129990,
+        disponibilidad: 5,
+        imagenUrl: 'https://www.ahstatic.com/photos/b5w1_rodbaofz_00_p_1024x768.jpg'
+      },
+      
   
     ];
   }
 
   agregarAlCarrito(habitacion: any) {
-    if (habitacion.cantidadDisponible > 0) {
+    if (habitacion.disponibilidad > 0) {
       console.log(`Habitación ${habitacion.nombre} añadida al carrito.`);
-      habitacion.cantidadDisponible--; 
+      habitacion.disponibilidad--; 
     } else {
       console.log(`Habitación ${habitacion.nombre} no está disponible.`);
     }
@@ -76,23 +112,4 @@ export class CatalogoHabitacionesComponent {
     this.router.navigate(['/habitacion', id]);
   }
 
-  desplazarIzquierda() {
-    this.indexActual--;
-
-    if (this.indexActual < 0) {
-      this.indexActual = this.habitaciones.length - 1; // Vuelve a la última habitación
-    }
-
-    this.desplazamiento = this.indexActual * this.anchoTarjeta; // Actualiza el desplazamiento
-  }
-
-  desplazarDerecha() {
-    this.indexActual++;
-
-    if (this.indexActual >= this.habitaciones.length) {
-      this.indexActual = 0; // Vuelve a la primera habitación
-    }
-
-    this.desplazamiento = this.indexActual * this.anchoTarjeta; // Actualiza el desplazamiento
-  }
 }
