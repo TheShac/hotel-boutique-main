d50495a0bec1973b043b8b5b0f5faff788bb8efa
TheShac <147677187+TheShac@users.noreply.github.com>
2024-11-04 00:29:48 -0300

    Update auth.guard.ts

--- hotel-boutique-reservation/src/app/auth.guard.ts
+++ hotel-boutique-reservation/src/app/auth.guard.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (!this.authService.isAuthenticated()) {
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+
+    const expectedRoles: string[] = route.data['rol'];
+    const userRole = this.authService.getRole();
+
+    if (!userRole || !expectedRoles.includes(userRole)) {
       this.router.navigate(['/login']);
       return false;
     }
     return true;
   }
 }
+
