d18b89915c4739e4b2e62cf594f6dd9fc3bd96ef
Gabriel Pailamilla <gabrielpailamilla13724@gmail.com>
2024-11-24 01:54:06 -0300

    Actualizacion de bd
    Se poblo con los datos de empleado

--- Base de Datos/hotel.sql
+++ Base de Datos/hotel.sql
@@ -5,7 +5,7 @@ CREATE TABLE usuario
   id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  role ENUM('client', 'admin') NOT NULL,
+  role ENUM('client', 'admin','empleado') NOT NULL,
   PRIMARY KEY (id),
   UNIQUE (email)
 );
@@ -14,3 +14,4 @@ select * from usuario;
 
 insert into usuario values(1, 'admin@hotel.com', 'admin123', 'admin');
 insert into usuario values(2, 'cliente@hotel.com', 'cliente123', 'client');
+insert into usuario values(2, 'empleado@hotel.com', 'empleado123', 'empleado');
